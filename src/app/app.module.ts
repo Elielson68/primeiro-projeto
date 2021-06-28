9ca7f68be570dc1cc86577e76a0d3802cd3f0df7
Elielson68 <elielsonbr.com@gmail.com>
2021-06-28 15:50:36 -0300

    nome projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/primeiro/primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'primeiro-component',
+    template: `
+        <p>Márcio beija popo</p>
+    `
+})
+
+export class PrimeiroComponent { }
